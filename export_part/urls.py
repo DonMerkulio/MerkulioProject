95a4034423abbe57e84568bdbe11c3728da6fb01
Alex <133873788+DonMerkulio@users.noreply.github.com>
2025-09-09 11:33:40 +0300

    Update urls.py

--- export_part/urls.py
+++ export_part/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from export_part.views import DownloadFileView
+from export_part.views import DownloadFileView, DownloadFileDromView
 
 urlpatterns = [
-    path('<str:key>/<str:file_type>/', DownloadFileView.as_view(), name='download_file')
+    path('<str:key>/<str:file_type>/', DownloadFileView.as_view(), name='download_file'),
+    path('drom/<str:key>/<str:file_type>/', DownloadFileDromView.as_view(), name='download_file_drom')
 ]
